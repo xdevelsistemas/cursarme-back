bfbbd5da812e9c974a8a481b4d298eb9682a8aae
clayton@xdevel.com.br <clayton@xdevel.com.br>
2015-12-19 00:02:28 -0200

    adiciondo estado civil e documentacao

---
+++ models/enum/maritalStatus.js
@@ -0,0 +1,19 @@
+/**
+ * Created by clayton on 18/12/15.
+ */
+module.exports = {
+    values : {
+        single: 'solteiro',
+        married: 'casado',
+        living: 'comunhão',
+        separed: 'separado',
+        divorced: 'divorciado',
+        na: 'não informado'
+    },
+    options : ['single', 'married', 'living', 'separed', 'divorced', 'na']
+};
+
+
+
+
+
--- models/person.js
+++ models/person.js
@@ -15,6 +15,7 @@ function callModule() {
     const xDevSchema = require("lib/xDevEntity").xDevSchema;
     const AddressSchema = require("lib/address");
     const ContactSchema = require("lib/contact");
+    const maritalStatus = require("enum/maritalStatus");
 
 
 
@@ -26,12 +27,20 @@ function callModule() {
     /**
      * model Schema
      */
+    const DocSchema = new Schema({
+        description : { type: String, required: true },
+        imageUrl : { type: String, required: true }
+    });
+
     return  xDevSchema.extend({
-        name: String,
+        name: { type: String, required: true },
         address: [AddressSchema],
         cpf: { type: String, unique: true , required: true },
+        rg: { type: String, required: false },
         phone: { type: String , required: true },
         user: { type: Schema.Types.ObjectId, ref : 'User' , required: true },
-        contacts : [ContactSchema]
+        maritalStatus: { type: String , required: true , array: maritalStatus.options },
+        contacts : [ContactSchema],
+        documents: [DocSchema]
     });
 }

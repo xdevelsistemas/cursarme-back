3d322fe5b262ea82b0dece7152bdba04b5a4bc9c
douglasbolis <douglas.lima@xdevel.com.br>
2016-01-21 15:02:32 -0200

    Ajustado Grunt para testar o novo teste.

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,7 +18,7 @@ module.exports = function(grunt) {
                 options: {
                     reporter: 'spec'
                 },
-                src: ['test/**/*Spec.js']
+                src: ['test/unitFunctionSpec.js']
             }
         }
     });

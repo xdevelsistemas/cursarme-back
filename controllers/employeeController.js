5d693176a6d798cb1f6bd64a33591096e117948d
douglas <douglas.lima@xdevel.com.br>
2016-03-03 15:28:08 -0300

    Ajustado ifs dos controllers

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -7,12 +7,20 @@ module.exports = () => {
     const getClient = require('../services/getClient');
     const EmployeeModel = require('../models/multitenant/employee');
     const ValidValues = require("../services/validValues");
-    const ValidAddress = require("../services/validAddress");
+    const ValidAddress = require("../services/validAddress").validAddress;
     const sanitize = require('mongo-sanitize');
     const ObjectId = require('mongoose').Types.ObjectId;
 
 
-    let validPerms = (obj) => (!!obj.unit && !!obj.modules);
+    let validPerms = (list) => {
+        let isValid = true;
+
+        list.forEach(function(el) {
+            isValid = isValid && (!!el.unit && !!el.modules);
+        });
+
+        return isValid;
+    };
 
     /**
      * lista um funcionário
@@ -48,10 +56,10 @@ module.exports = () => {
      * @param res
      */
     employeeController.add = (req, res) => {
-        if (!(req.body.admin && req.body.enabled && req.body.position && req.body.titration && req.body.perms
-        && validPerms(req.body.perms) && req.body.name && req.body.birthDate && req.body.cpf && req.body.phones
-        && req.body.user && req.body.maritalStatus && req.body.gender && req.body.ethnicity && req.body.contacts
-        && req.body.documents && (req.body.address.length === 0) && req.body.address && ValidAddress(req.body.address))) {
+        if (!req.body.admin || !req.body.enabled || !req.body.position || !req.body.titration || !req.body.perms
+        || !validPerms(req.body.perms) || !req.body.name || !req.body.birthDate || !req.body.cpf || !req.body.phones
+        || !req.body.user || !req.body.maritalStatus || !req.body.gender || !req.body.ethnicity || !req.body.contacts
+        || !req.body.documents || !req.body.address || (req.body.address.length === 0) || !ValidAddress(req.body.address)) {
             return MongooseErr.apiCallErr("Dados inválidos", res, 400);
         }
 
@@ -73,11 +81,11 @@ module.exports = () => {
      * @param res
      */
     employeeController.update = (req, res) => {
-        if (!(ObjectId.isValid(sanitize(req.body._id)) && req.body.admin && req.body.enabled && req.body.position
-        && req.body.titration && req.body.perms && validPerms(req.body.perms) && req.body.name && req.body.birthDate
-        && req.body.cpf && req.body.phones && req.body.user && req.body.maritalStatus && req.body.gender
-        && req.body.ethnicity && req.body.contacts && req.body.documents && (req.body.address.length === 0)
-        && req.body.address && ValidAddress(req.body.address))) {
+        if (!ObjectId.isValid(sanitize(req.body._id)) || !req.body.admin || !req.body.enabled || !req.body.position
+        || !req.body.titration || !req.body.perms || !validPerms(req.body.perms) || !req.body.name || !req.body.birthDate
+        || !req.body.cpf || !req.body.phones || !req.body.user || !req.body.maritalStatus || !req.body.gender
+        || !req.body.ethnicity || !req.body.contacts || !req.body.documents || (req.body.address.length === 0)
+        || !req.body.address || !ValidAddress(req.body.address)) {
             return MongooseErr.apiCallErr("Dados inválidos", res, 400);
         }
 
--- controllers/unitController.js
+++ controllers/unitController.js
@@ -8,7 +8,7 @@ module.exports = () => {
     const MongooseErr = require('../services/MongooseErr');
     const getClient = require('../services/getClient');
     const UnitModel = require('../models/multitenant/unit');
-    const ValidAddress = require("../services/validAddress");
+    const ValidAddress = require("../services/validAddress").validAddress;
     const sanitize = require('mongo-sanitize');
     const ObjectId = require('mongoose').Types.ObjectId;
 
@@ -49,14 +49,15 @@ module.exports = () => {
      * @param res
      */
     unitController.add = (req, res) => {
-        if (!(req.body.name && req.body.address && req.body.cnpj && req.body.alias && req.body.phone
-        && req.body.website && req.body.directorAuthorization && req.body.secretaryAuthorization
-        && ValidAddress(req.body.address))) {
+        if (!req.body.name || !req.body.address || !req.body.cnpj || !req.body.alias || !req.body.phone
+        || !req.body.website || !req.body.directorAuthorization || !req.body.secretaryAuthorization
+        || !ValidAddress(req.body.address)) {
             return MongooseErr.apiCallErr("Dados inválidos", res, 400);
         }
 
-        req.body.director = ObjectId(req.body.director);
-        req.body.secretary = ObjectId(req.body.secretary);
+        // convertendo os ids para ObjectId;
+        req.body.director = ObjectId.isValid(req.body.director) ? ObjectId(req.body.director) : req.body.director;
+        req.body.secretary = ObjectId.isValid(req.body.secretary) ? ObjectId(req.body.secretary) : req.body.secretary;
 
         return UnitModel(getClient(req)).add(req.user._id, true, 'Test', req.body)
             .then((data) => res.status(201).json(data))
@@ -69,15 +70,15 @@ module.exports = () => {
      * @param res
      */
     unitController.update = (req, res) => {
-        if (!(ObjectId.isValid(sanitize(req.body._id)) && req.body.name && req.body.address && req.body.cnpj && req.body.alias && req.body.phone
-        && req.body.website && req.body.directorAuthorization && req.body.secretaryAuthorization
-        && (req.body.address.length === 0) && ValidAddress(req.body.address))) {
+        if (!ObjectId.isValid(sanitize(req.body._id)) || !req.body.name || !req.body.address || !req.body.cnpj || !req.body.alias || !req.body.phone
+        || !req.body.website || !req.body.directorAuthorization || !req.body.secretaryAuthorization
+        || (req.body.address.length === 0) || !ValidAddress(req.body.address)) {
             return MongooseErr.apiCallErr("Dados inválidos", res, 400);
         }
 
         // convertendo os ids para ObjectId;
-        req.body.director = ObjectId(req.body.director);
-        req.body.secretary = ObjectId(req.body.secretary);
+        req.body.director = ObjectId.isValid(req.body.director) ? ObjectId(req.body.director) : req.body.director;
+        req.body.secretary = ObjectId.isValid(req.body.secretary) ? ObjectId(req.body.secretary) : req.body.secretary;
 
         return UnitModel(getClient(req)).update(req.user._id, true, 'Test', req.body)
             .then((data) => res.status(200).json(data))
